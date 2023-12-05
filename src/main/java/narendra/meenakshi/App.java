050626f7f9316c753f0b068f62b03369d4ea8c5a
meena <meena@Meenus>
2023-12-05 13:14:37 +0530

    narendra is a bad boy

---
+++ src/main/java/narendra/meenakshi/App.java
@@ -0,0 +1,13 @@
+package narendra.meenakshi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
